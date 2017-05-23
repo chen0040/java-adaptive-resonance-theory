d96e052054940deafa01e97321212cd3cc34b077
xschen <xs0040@gmail.com>
2017-05-24 03:33:24 +0800

    Update docs

--- README.md
+++ README.md
@@ -16,6 +16,54 @@ Applications included:
 * Clustering (FuzzyART, ART1)
 * Multi-class Classification (ARTMAP)
 
+# Usage
+
+### Multi-class Classification using ARTMAP
+
+To create and train a ARTMAP classifier:
+
+```java
+ARTMAPClassifier classifier = new ARTMAPClassifier();
+clasifier.fit(trainingData);
+```
+
+The "trainingData" is a data frame which holds data rows with labeled output (Please refers to this link to find out how to store data into a data frame)
+
+To predict using the trained ARTMAP classifier:
+
+```java
+String predicted_label = classifier.transform(dataRow);
+```
+
+The detail on how to use this can be found in the unit testing codes. Below is a complete sample codes of classifying on the libsvm-formatted heart-scale data:
+
+```java
+InputStream inputStream = new FileInputStream("heart_scale");
+DataFrame dataFrame = DataQuery.libsvm().from(inputStream).build();
+
+// as the dataFrame obtained thus far has numeric output instead of labeled categorical output, the code below performs the categorical output conversion
+dataFrame.unlock();
+for(int i=0; i < dataFrame.rowCount(); ++i){
+ DataRow row = dataFrame.row(i);
+ row.setCategoricalTargetCell("category-label", "" + row.target());
+}
+dataFrame.lock();
+
+
+classifier.setAlpha(alpha);
+classifier.setBeta(beta);
+classifier.setRho0(rho);
+
+classifier.fit(dataFrame);
+
+for(int i = 0; i < dataFrame.rowCount(); ++i){
+  DataRow tuple = dataFrame.row(i);
+  String predicted_label = classifier.transform(tuple);
+  System.out.println("predicted: "+predicted_label+"\tactual: "+tuple.categoricalTarget());
+}
+
+```
+
 
 
 
--- src/test/java/com/github/chen0040/art/classifiers/ARTMAPClassifierUnitTest.java
+++ src/test/java/com/github/chen0040/art/classifiers/ARTMAPClassifierUnitTest.java
@@ -24,46 +24,46 @@ public class ARTMAPClassifierUnitTest {
    public void TestHeartScale() throws FileNotFoundException {
       InputStream inputStream = FileUtils.getResource("heart_scale");
 
-      DataFrame batch = DataQuery.libsvm().from(inputStream).build();
+      DataFrame dataFrame = DataQuery.libsvm().from(inputStream).build();
 
-      batch.unlock();
-      for(int i=0; i < batch.rowCount(); ++i){
-         DataRow row = batch.row(i);
+      dataFrame.unlock();
+      for(int i=0; i < dataFrame.rowCount(); ++i){
+         DataRow row = dataFrame.row(i);
          row.setCategoricalTargetCell("category-label", "" + row.target());
       }
-      batch.lock();
+      dataFrame.lock();
 
 
       double best_alpha=0, best_beta=0, best_rho_base=0;
       double predictionAccuracy = 0;
-      int nodeCount = batch.rowCount();
+      int nodeCount = dataFrame.rowCount();
 
       for(double alpha = 8; alpha < 10; alpha += 0.1) {
          for(double beta = 0; beta < 0.5; beta += 0.1) {
             for(double rho = 0.01; rho < 0.05; rho += 0.1){
-               ARTMAPClassifier svc = new ARTMAPClassifier();
+               ARTMAPClassifier classifier = new ARTMAPClassifier();
 
-               svc.setAlpha(alpha);
-               svc.setBeta(beta);
-               svc.setRho0(rho);
+               classifier.setAlpha(alpha);
+               classifier.setBeta(beta);
+               classifier.setRho0(rho);
 
-               svc.fit(batch);
+               classifier.fit(dataFrame);
 
                int correctnessCount = 0;
-               for(int i = 0; i < batch.rowCount(); ++i){
-                  DataRow tuple = batch.row(i);
-                  String predicted_label = svc.transform(tuple);
+               for(int i = 0; i < dataFrame.rowCount(); ++i){
+                  DataRow tuple = dataFrame.row(i);
+                  String predicted_label = classifier.transform(tuple);
                   //System.out.println("predicted: "+predicted_label+"\texpected: "+tuple.getLabelOutput());
                   correctnessCount += (predicted_label.equals(tuple.categoricalTarget()) ? 1 : 0);
                }
 
-               double accuracy = (correctnessCount * 100 / batch.rowCount());
-               if((accuracy > predictionAccuracy && nodeCount / (double)svc.nodeCount() > 0.8) || (accuracy / predictionAccuracy > 0.85 && nodeCount > svc.nodeCount())){
+               double accuracy = (correctnessCount * 100 / dataFrame.rowCount());
+               if((accuracy > predictionAccuracy && nodeCount / (double)classifier.nodeCount() > 0.8) || (accuracy / predictionAccuracy > 0.85 && nodeCount > classifier.nodeCount())){
                   best_alpha = alpha;
                   best_beta = beta;
                   best_rho_base = rho;
                   predictionAccuracy = accuracy;
-                  nodeCount = svc.nodeCount();
+                  nodeCount = classifier.nodeCount();
                }
 
                //System.out.println("Prediction accuracy: " + (correctnessCount * 100 / batch.size()));
@@ -74,7 +74,7 @@ public class ARTMAPClassifierUnitTest {
       System.out.println("QAlpha: "+best_alpha + "\tbeta: "+best_beta+"\trho_base: "+best_rho_base);
       System.out.println("accuracy: "+predictionAccuracy);
       System.out.println("nodeCount: "+nodeCount);
-      System.out.println("dbSize: "+batch.rowCount());
+      System.out.println("dbSize: "+dataFrame.rowCount());
 
 
 
